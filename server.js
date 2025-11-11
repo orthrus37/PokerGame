449c0e94e55db5c3f1290528ae4ca4c7a3359bf8
orthrus37 <140781664+orthrus37@users.noreply.github.com>
2025-11-11 17:14:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const SMALL_BLIND = 25;
 const BIG_BLIND = 50;
 
 const SUITS = ['♠','♥','♦','♣'];
-const RANKS = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+const RANKS = ['2','3','4','5','6','7','8','9','T','J','Q','K','A'];
 
 // Watchdog: if no action/advance for this many ms, auto-advance stage
 const WATCHDOG_MS = 45000;
